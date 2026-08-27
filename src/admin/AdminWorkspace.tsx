71a44acca9e5f2d1963e151460c7ec93bed4dd02
fedotovadancephoto-eng <fedotova.dance.photo@gmail.com>
2026-08-27 12:24:34 +0800

    Подключить управление оплатой

--- src/admin/AdminWorkspace.tsx
+++ src/admin/AdminWorkspace.tsx
@@ -1,6 +1,7 @@
 import AdminApp from "@/admin/AdminApp";
 import { AdminCoinManager } from "@/admin/AdminCoinManager";
 import { AdminNewsManager } from "@/admin/AdminNewsManager";
+import { AdminPaymentManager } from "@/admin/AdminPaymentManager";
 import { AdminScheduleManager } from "@/admin/AdminScheduleManager";
 import { AdminStudentOperations } from "@/admin/AdminStudentOperations";
 import { AdminStudyManager } from "@/admin/AdminStudyManager";
@@ -16,6 +17,7 @@ export default function AdminWorkspace() {
       <AdminStudyManager />
       <AdminCoinManager />
       <AdminNewsManager />
+      <AdminPaymentManager />
     </>
   );
 }
