629da2bb42db6e8d18041281a7e01b373c00fe6a
fedotovadancephoto-eng <fedotova.dance.photo@gmail.com>
2026-08-27 09:08:22 +0800

    Включить автоматический повтор запросов Supabase

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { installSupabaseFetchGuard } from './supabaseFetchGuard';
 import './index.css';
+
+installSupabaseFetchGuard();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
